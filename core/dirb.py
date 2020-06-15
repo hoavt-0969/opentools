e8f4a41e10cef680d87b3fb8980a4b8b2ee62d71
hoavt-0969 <vu.tien.hoa@sun-asterisk.com>
2020-06-15 10:45:18 +0700

    convert class

--- core/dirb.py
+++ core/dirb.py
@@ -1,84 +1,132 @@
 import requests
-import time
 import threading
-import urllib.parse
 from queue import Queue
-from core import config
 import sys
-# url = "http://testphp.vulnweb.com"
-resume = None
-proxies = {}
-cookies = config.cookies
-url = config.url
-# wordlist_file ="../subdomains.txt"
-
-number_threads = config.number_threads
-wordlist_file = config.wordlist
-extensions = config.extensions
-raw_words = []
-def build_wordlist(wordlist_file):
-    # try:
-    #     raw_words = open(wordlist_file,'r',errors="replace",encoding='utf-8').readlines()
-    # except Exception as e:
-    #     print(e)
-    #     sys.exit(1)
-    f = open(wordlist_file,"r")
-    raw_word = f.readline()
-    while raw_word != '':
+import urllib
+class Dirb(object):
+    def __init__(self, url, extensions, wordlist, cookies=None, headers=None,threads=10,proxies=None):
+        self.url = url
+        self.extensions = extensions
+        self.wordlist = wordlist
+        self.cookies = cookies
+        self.headers = headers
+        self.threads = threads
+        self.proxies = proxies
+        self.resume = None
+        self.words = Queue()
+    def load_wordlist(self):
         try:
-            raw_word = f.readline()
-            raw_words.append(raw_word)
-        except:
-            pass
-    f.close()
-    found_resume = False
-    words = Queue()
-    for word in raw_words:
-        word = word.rstrip()
-        if resume is not None:
-            if found_resume:
-                words.put(word)
+            raw_data = open(self.wordlist,'r',errors="replace").read().splitlines()
+        except Exception as e:
+            print(e)
+            sys.exit(1)
+        return raw_data
+    
+    def build_wordlist(self):
+        found_resume = False
+        raw_words = self.load_wordlist()
+        # words = Queue()
+        for word in raw_words:
+            word = word.rstrip()
+            if self.resume is not None:
+                if found_resume:
+                    self.words.put(word)
+                else:
+                    if word == self.resume:
+                        found_resume = True
             else:
-                if word == resume:
-                    found_resume = True
+                self.words.put(word)
+        
+        return self.words
+
+    def Scanner(self,word_queue):
+        # print('hello')
+        while not word_queue.empty():
+            attempt = word_queue.get()
+            attempt_list = []
+            if "." not in attempt:
+                attempt_list.append("/%s/" %attempt)
+            else:
+                attempt_list.append("/%s/" %attempt)
+            if self.extensions:
+                for extension in self.extensions:
+                    attempt_list.append("/%s%s"%(attempt,extension))
+            for brute in attempt_list:
+                url = "%s%s" %(self.url,brute)
+                # print(brute)
+                # print(url)
+                try:
+                    res = requests.get(url,cookies=self.cookies, allow_redirects=False, proxies=self.proxies)
+                    if res.status_code !=404:
+                        print("+ [%d] - %s"%(res.status_code,url))
+                except Exception as e:
+                    print("Error requests")
+                # print(vars(res))
+                # if res.status_code !=404:
+                #     print("+ [%d] - %s"%(res.status_code,url))
+        word_queue.task_done()
+
+    def run(self):
+        # print(vars(self.build_wordlist()))
+        for thread in range(self.threads):
+            # t = threading.Thread(target=self.Scanner,args=(self.build_wordlist()))
+            t = threading.Thread(target=self.Scanner,args=(self.build_wordlist(),))
+            # t.daemon = True
+            t.start()
+            # t.join()
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(prog="scan")
+    subparsers = parser.add_subparsers(dest='command')
+
+    dirb_parser = subparsers.add_parser("dirb")
+    xss_parser = subparsers.add_parser("xss")
+    sub_parser = subparsers.add_parser("sub")
+
+    dirb_parser.add_argument('-u','--url',required=True,type=str,default=None)
+    dirb_parser.add_argument('-w','--wordlist',type=str,required=True,default="/home/sun/opentools/subdomains.txt")
+    dirb_parser.add_argument("-e","--extensions",type=str,required=True,default=".php,.html,.txt")
+    dirb_parser.add_argument("-t", "--threads",default=10,type=int,help="Set number threads", required=False)
+    dirb_parser.add_argument('--cookies', required=False,type=str)
+
+    xss_parser.add_argument('-u','--url',required=True,type=str)
+    xss_parser.add_argument('--cookies', required=False,type=str)
+
+    sub_parser.add_argument('-d', '--domain', required=True, type=str)
+    sub_parser.add_argument('-t', '--threads', required=False, type=int, default=10)
+    return parser.parse_args()
+
+def parse_cookie(rawdata):
+    if rawdata == None:
+        return rawdata
+    from http.cookies import SimpleCookie
+    cookie = SimpleCookie()
+    cookie.load(rawdata)
+    cookies = {}
+    for key, data in cookie.items():
+        cookies[key] = data.value
+    return cookies
+
+if __name__ == "__main__":
+    # url = "http://testphp.vulnweb.com"
+    # threads = 100
+    # extensions = ['.php','.html','.txt']
+    # wordlist = "/usr/share/wordlists/dirb/big.txt"
+    # scanner = Dirb(url=url,extensions=extensions,wordlist=wordlist,threads=threads)
+    # scanner.run()
+    args = parse_args()
+    print(vars(args))
+    if args.command == "dirb":
+        if args.url[-1] == "/":
+            url = args.url[:-1]
         else:
-            words.put(word)
-    return words
-
-
-def dirb(word_queue, extensions=None):
-    while not word_queue.empty():
-        attempt = word_queue.get()
-        attempt_list = []
-        if "." not in attempt:
-            attempt_list.append("/%s/" % attempt)
-        else:
-            attempt_list.append("/%s/" % attempt)
-        if extensions:
-            for extension in extensions:
-                attempt_list.append("/%s%s" % (attempt,extension))
-        for brute in attempt_list:
-            rq_url = "%s%s"%(url,brute)
-            # print(rq_url)
-            u = urllib.parse.urlparse(rq_url)
-            u = u.geturl()
-            # print(u.geturl())
-            try:
-                r = requests.get(u,proxies=proxies,cookies=cookies,allow_redirects =False)
-                print("+ [%d] - %s"%(r.status_code,u))
-            except:
-                print("Error request")
-                # print(e)
-            
-            # if r.status_code != 404:           
-            #     print("+ [%d] - %s"%(r.status_code,u))
-
-word_queue = build_wordlist(wordlist_file)
-
-# while not word_queue.empty():
-#     print(word_queue.get())
-
-def main():
-    for i in range(number_threads):
-        t = threading.Thread(target=dirb,args=(word_queue,extensions,))
-        t.start()
+            url = args.url
+    # print(url[-1])
+    # print(url)
+        threads = args.threads
+        cookies = parse_cookie(args.cookies)
+        wordlist = args.wordlist
+        extensions = args.extensions.split(",")
+        scanner = Dirb(url=url, extensions=extensions, wordlist=wordlist, threads=threads,cookies=cookies)
+        scanner.run()
